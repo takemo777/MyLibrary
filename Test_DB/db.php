b637b5a2f0e43a418e94b7c7f79f880881f8446d
kanamun-kbc <ggl.skanamun.199892.6701@gmail.com>
2023-06-30 10:52:54 +0900

    kakunin2

--- Test_DB/db.php
+++ Test_DB/db.php
@@ -6,7 +6,7 @@ class DAO
 {
 
     private $user = "root";
-    private $pwd = "";
+    private $pwd = "pathSQL";
     private $dsn = "mysql:host=localhost;port=3306;dbname=library;";
     private $conn;
 
